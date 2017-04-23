6145c8ace13008de18ecfe3eeb72858538f7b218
Matt Palumbo <DukGwang@Matthews-iMac.local>
2017-04-23 07:18:43 -0400

    configured cors on server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const {PORT, DATABASE_URL} = require('./config');
 const {Threads} = require('./models');
@@ -14,6 +15,7 @@ const router = require('./routes/threadsRouter')
 mongoose.Promise = global.Promise;
 
 const app = express();
+app.use(cors());
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(express.static('public'));
