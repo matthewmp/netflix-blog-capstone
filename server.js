59e700af53417d4ecf9d434a6f9bae1064d8dd74
miclovich <vicmiclovich@gmail.com>
2017-06-07 16:44:38 +0300

    move variables around

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-
-
+const passport = require('passport');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const BasicStrategy = require('passport-http').BasicStrategy;
 const {PORT, DATABASE_URL} = require('./config');
-
+const { User } = require('./models/userModel');
 const {router: usersRouter} = require('./users');
 
 const threadsRouter = require('./routes/threadsRouter');
@@ -18,13 +17,66 @@ mongoose.Promise = global.Promise;
 
 const app = express();
 
-app.use('/users/', usersRouter);
+var user_cache = {};
 
+// const { basicStrategy } = require('./routes/userRouter');
+
+const basicStrategy = new BasicStrategy((username, password, callback) => {
+  let user;
+  User
+    .findOne({ username: username })
+    .exec()
+    .then(_user => {
+      user = _user;
+      if (!user) {
+        return callback(null, false, { message: 'Incorrect username' });
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return isValid.callback(null, false, { message: 'Incorrect password' });
+      }
+      else {
+        return callback(null, user)
+      }
+    })
+});
+
+
+passport.use(basicStrategy);
+// passport.use(new BasicStrategy(
+//   function(userid, password, done) {
+//     User.findOne({ username: userid }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) { return done(null, false); }
+//       if (!user.verifyPassword(password)) { return done(null, false); }
+//       return done(null, user);
+//     });
+//   }
+// ));
+
+passport.serializeUser(function (user, next) {
+  let id = user._id;
+  user_cache[id] = user;
+
+  console.log(`USER: ${user}`)
+  next(null, id);
+});
+
+passport.deserializeUser(function (user, next) {
+  next(null, user_cache[id]);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+
+app.use('/users/', usersRouter);
 app.use('/threads', threadsRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
@@ -78,5 +130,5 @@ if(require.main === module){
 };
 
 
-module.exports = {runServer, closeServer, app};
+module.exports = {runServer, closeServer, app, passport};
 
